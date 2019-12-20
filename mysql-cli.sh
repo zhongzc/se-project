fc7d70b4d19ac24ad4a149099aa22a5bf116d21d
zhongzc <zhongzc_arch@outlook.com>
2019-12-20 20:07:39 +0800

    init

--- mysql-cli.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. ./.env
-
-docker-compose run --rm mysql     \
-  mysql --host=mysql              \
-        --user="$DB_USER"         \
-        --password="$DB_PASSWORD" \
-        --database="$DB_DATABASE"
--- sql/create-tables.sql
+++
@@ -1,7 +0,0 @@
-create table books
-(
-    id     mediumint   not null auto_increment,
-    title  varchar(20) not null,
-    author varchar(20) not null,
-    primary key (id)
-);
